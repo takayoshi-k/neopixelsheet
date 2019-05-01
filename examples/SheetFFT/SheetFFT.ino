c214dd781ee635810a743652036922365cf4acfc
takayoshi.koizumi <takayoshi.koizumi@jp.sony.com>
2019-05-02 05:50:20 +0900

    Add use of adjustment APIs on FFTScene.

--- examples/SheetFFT/SheetFFT.ino
+++ examples/SheetFFT/SheetFFT.ino
@@ -23,7 +23,26 @@ void setup() {
 
     setData(0.0);
 
+    // Set your original colors.
+    myScene.setLevelColor(0, 0x00002020);
+    myScene.setLevelColor(1, 0x00000030);
+    myScene.setLevelColor(2, 0x00303000);
+    myScene.setLevelColor(3, 0x00103000);
+    myScene.setLevelColor(4, 0x00006000);
+
+    // Cnahge average steps.
+    myScene.setAvarageStep(2);
+
+    // Cnahge bias of avaraged data.
+    myScene.setBias(1.0);
+
+    // Change Indicator Unit number per one step.
+    myScene.setIndicatorUnit(4);
+
+    // Set dummy data.
     myScene.updateFFT(data);
+
+    // Draw data on the Target.
     myScene.draw();
 
     usleep(50);
